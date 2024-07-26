ca3fe1180962da23aa12e56d4a94f4a2ecd1e0c3
Fiell75785 <Alexandr.zub@mail.ru>
2024-07-26 14:56:39 +0300

    Add files via upload

---
+++ js/img.js
@@ -0,0 +1,10 @@
+// $(function(){
+
+//     $(function(){
+//   var images = ['url(../img/1.jpg)', 'url(../img/1.jpg))', 'url(../img/1.jpg))', 'url(../img/1.jpg))', 'url(../img/1.jpg))'];
+//   var currentImage = 0;
+//    $('.slider img').css(
+//       {'background-image': 'images[currentImage]'}
+//     );
+//   });
+// })
---
+++ js/slider.js
@@ -0,0 +1,47 @@
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Добавляем функцию для автоматического переключения слайдов каждые 10 секунд
+function autoUpdateSlider() {
+    setInterval(() => {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+    }, 10000); // 10 секунд
+    }
+    
+    // Запускаем автоматическое обновление слайдера
+    autoUpdateSlider();
+// Инициализация слайдера
+updateSlider();
